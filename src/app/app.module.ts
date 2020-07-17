3b18b515f6770beb323a9785e6e7f23b1dd3e124
Wombat <b3wombat@gmail.com>
2020-07-17 10:10:30 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProjectComponent } from './main/project/project.component';
+import { AboutComponent } from './main/about/about.component';
+import { SkillsComponent } from './main/skills/skills.component';
+import { CareerComponent } from './main/career/career.component';
+import { ContactComponent } from './main/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ProjectComponent,
+    AboutComponent,
+    SkillsComponent,
+    CareerComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
